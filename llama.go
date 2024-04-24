05d3ceb29d489d12a60772646c79bbb4e6adbd65
dyzhuu <119282717+dyzhuu@users.noreply.github.com>
2024-04-25 05:06:38 +1200

    ui

--- llama.go
+++ llama.go
@@ -9,42 +9,42 @@ import (
 )
 
 type LlamaResponse struct {
-	Model    string    `json:"model"`
-	CreatedAt string `json:"created_at"`
-	Response  string    `json:"response"`
-	Done	  bool      `json:"done"`
-	Context   []int `json:"context"`
-	TotalDuration int `json:"total_duration"`
-	LoadDuration int `json:"load_duration"`
-	PromptEvalDuration int `json:"prompt_eval_duration"`
-	EvalCount int `json:"eval_count"`
-	EvalDuration int `json:"eval_duration"`
+	Model              string `json:"model"`
+	CreatedAt          string `json:"created_at"`
+	Response           string `json:"response"`
+	Done               bool   `json:"done"`
+	Context            []int  `json:"context"`
+	TotalDuration      int    `json:"total_duration"`
+	LoadDuration       int    `json:"load_duration"`
+	PromptEvalDuration int    `json:"prompt_eval_duration"`
+	EvalCount          int    `json:"eval_count"`
+	EvalDuration       int    `json:"eval_duration"`
 }
 
 const url string = "http://localhost:11434/api/generate"
 
-func Request(prompt string, dataChannel chan <- string) {
+func StreamRequest(prompt string, dataChannel chan<- string) {
 	jsonBody := []byte(`{
 		"model": "llama3:8b",
-		"prompt": "`+prompt+`"
+		"prompt": "` + prompt + `"
 	}`)
 
 	bodyReader := bytes.NewReader(jsonBody)
-	
+
 	resp, err := http.Post(url, "application/json", bodyReader)
-	
+
 	if err != nil {
 		fmt.Printf("error making http request: %s\n", err)
 		os.Exit(1)
 	}
 
 	defer resp.Body.Close()
-	
+
 	var response LlamaResponse
 
 	json.NewDecoder(resp.Body).Decode(&response)
 	dataChannel <- response.Response
-	for (!response.Done) {
+	for !response.Done {
 		json.NewDecoder(resp.Body).Decode(&response)
 		dataChannel <- response.Response
 	}
--- main.go
+++ main.go
@@ -2,15 +2,125 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
-func main() {
-	// message := os.Args[1]
+func getData(m *model, message string) {
 	dataChannel := make(chan string)
-	
-	go Request("hi", dataChannel)
 
+	go StreamRequest(message, dataChannel)
+
+	// m.response = ""
 	for data := range dataChannel {
-		fmt.Print(data)
+		print(data)
+		// m.response = m.response + data
 	}
+}
+
+func main() {
+	p := tea.NewProgram(initialModel())
+
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type (
+	errMsg error
+)
+
+type model struct {
+	viewport    viewport.Model
+	messages    []string
+	textarea    textarea.Model
+	width       int
+	height      int
+	senderStyle lipgloss.Style
+	err         error
+}
+
+func initialModel() model {
+	ta := textarea.New()
+	ta.Placeholder = "Send a message..."
+	ta.Focus()
+
+	ta.Prompt = "┃ "
+	ta.CharLimit = 5000
+
+	ta.SetHeight(2)
+
+	ta.FocusedStyle.CursorLine = lipgloss.NewStyle()
+
+	ta.ShowLineNumbers = false
+
+	vp := viewport.New(30, 5)
+	vp.SetContent(`Welcome to the chat room!
+Type a message and press Enter to send.`)
+
+	ta.KeyMap.InsertNewline.SetEnabled(false)
+
+	return model{
+		textarea:    ta,
+		messages:    []string{},
+		viewport:    vp,
+		senderStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("5")),
+		err:         nil,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textarea.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		tiCmd tea.Cmd
+		vpCmd tea.Cmd
+	)
+
+	m.textarea, tiCmd = m.textarea.Update(msg)
+
+	m.textarea.SetHeight((m.textarea.Length())/m.textarea.Width() + 2)
+
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.height = msg.Height
+		m.width = msg.Width
+		m.textarea.SetWidth(msg.Width)
+		m.viewport.Height = msg.Height - m.textarea.Height() - 1
+		m.viewport.Width = msg.Width
+		return m, nil
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		case tea.KeyEnter:
+			m.messages = append(m.messages, m.senderStyle.Render("You: ")+m.textarea.Value())
+			m.textarea.Reset()
+			m.textarea.SetHeight(2)
+			m.viewport.SetContent(strings.Join(m.messages, "\n"))
+			m.viewport.GotoBottom()
+
+		}
+
+	case errMsg:
+		m.err = msg
+		return m, nil
+	}
+
+	return m, tea.Batch(tiCmd, vpCmd)
+}
+
+func (m model) View() string {
+	return fmt.Sprintf(
+		"%s\n\n%s",
+		m.viewport.View(),
+		m.textarea.View(),
+	)
 }
